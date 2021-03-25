462f89fd677675434fc0717dadba6de8ffba4454
nachiket59 <pethenachiket5@gmail.com>
2021-03-25 12:01:51 +0530

    thread list and tcb

--- thread_list.c
+++ thread_list.c
@@ -64,54 +64,88 @@ void tlist_insert_begin(thread_list** start, thread_control_block tcb)
 
 void tlist_display(thread_list* start)
 {
+	if(start == NULL){
+		printf("No nodes\n");
+		return;
+	}
 	thread_list *temp = start;
 
 	printf("\nTraversal in forward direction \n");
 	while (temp->next != start)
 	{
-		printf("%d ", temp->data);
+		printf("%d ", temp->tcb.tid);
 		temp = temp->next;
 	}
-	printf("%d ", temp->data);
+	printf("%d ", temp->tcb.tid);
 
 	printf("\nTraversal in reverse direction \n");
 	thread_list* last = start->prev;
 	temp = last;
 	while (temp->prev != last)
 	{
-		printf("%d ", temp->data);
+		printf("%d ", temp->tcb.tid);
 		temp = temp->prev;
 	}
-	printf("%d ", temp->data);
+	printf("%d ", temp->tcb.tid);
 }
 
-/*
-int main()
+
+void tlist_delete(thread_list** start, int key)
 {
-	
-	thread_list* start = NULL;
-
-	// Insert 5. So linked list becomes 5->NULL
-	insertEnd(&start, 5);
-
-	// Insert 4 at the beginning. So linked 
-	// list becomes 4->5
-	insertBegin(&start, 4);
-
-	// Insert 7 at the end. So linked list
-	// becomes 4->5->7
-	insertEnd(&start, 7);
-
-	// Insert 8 at the end. So linked list 
-	// becomes 4->5->7->8
-	insertEnd(&start, 8);
-
-	// Insert 6, after 5. So linked list 
-	// becomes 4->5->6->7->8
-	insertAfter(&start, 6, 5);
-
-	printf("Created circular doubly linked list is: ");
-	display(start);
-
-	return 0;
-}*/
+    // If list is empty
+    if (*start == NULL)
+        return;
+  
+    // Find the required node
+    // Declare two pointers and initialize them
+    thread_list *curr = *start, *prev_1 = NULL;
+    while (curr->tcb.tid != key) {
+        // If node is not present in the list
+        if (curr->next == *start) {
+            printf("\nList doesn't have node with value = %d", key);
+            return;
+        }
+  
+        prev_1 = curr;
+        curr = curr->next;
+    }
+  
+    // Check if node is the only node in list
+    if (curr->next == *start && prev_1 == NULL) {
+        (*start) = NULL;
+        free(curr);
+        return;
+    }
+  
+    // If list has more than one node,
+    // check if it is the first node
+    if (curr == *start) {
+        // Move prev_1 to last node
+        prev_1 = (*start)->prev;
+  
+        // Move start ahead
+        *start = (*start)->next;
+  
+        // Adjust the pointers of prev_1 and start node
+        prev_1->next = *start;
+        (*start)->prev = prev_1;
+        free(curr);
+    }
+  
+    // check if it is the last node
+    else if (curr->next == *start) {
+        // Adjust the pointers of prev_1 and start node
+        prev_1->next = *start;
+        (*start)->prev = prev_1;
+        free(curr);
+    }
+    else {
+        // create new pointer, points to next of curr node
+        thread_list* temp = curr->next;
+  
+        // Adjust the pointers of prev_1 and temp node
+        prev_1->next = temp;
+        temp->prev = prev_1;
+        free(curr);
+    }
+}
